441a9046346300070b0552b4b1aa1bc87cc07a67
Irina <pamir.08@mail.ru>
2022-10-11 19:04:10 +0300

    ex4

---
+++ ex4/Program.cs
@@ -0,0 +1,14 @@
+﻿//Показать кубы чисел, заканчивающихся на четную цифру
+Console.WriteLine ("Введите число N");
+int N = int.Parse (Console.ReadLine ());
+int b = 0;
+int c = 1;
+while (b <= N)
+{
+    c = b * b * b;
+    if (c % 2 == 0)
+    {
+        Console.WriteLine ($"Куб числа {b} равен {c}");
+    }
+    b++;
+}
