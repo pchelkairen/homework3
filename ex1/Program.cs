7a1962ba8c85e5cbfc545bd35b0cdd6e82cb4da9
Irina <pamir.08@mail.ru>
2022-10-11 14:05:21 +0300

    ex1

---
+++ ex1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine ("Введите число N");
+int N = int.Parse (Console.ReadLine ());
+int a = 1;
+while (a <= N)
+{
+    int b = a * a * a;
+    Console.WriteLine ($"a = {a}, куб числа a = {b}");
+    a++;
+}
