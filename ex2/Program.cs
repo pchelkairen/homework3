1e7e6eb2a232f5e5c1a55b4d525709562c6a46f7
Irina <pamir.08@mail.ru>
2022-10-11 14:31:42 +0300

    ex2

---
+++ ex2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Подсчитать сумму цифр в числе
+Console.WriteLine ("Введите число A");
+int A = int.Parse (Console.ReadLine ());
+int sum = 0;
+while (A > 0)
+{
+    sum = sum + A%10;
+    A = A / 10;
+}
+Console.WriteLine ("Cумма цифр в числе " +sum);
