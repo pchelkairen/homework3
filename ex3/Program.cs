94c7c71fd5ddf2c75cf5e33ce29c89eed8d351be
Irina <pamir.08@mail.ru>
2022-10-11 15:09:02 +0300

    ex3

---
+++ ex3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.WriteLine ("Введите число N");
+int N = int.Parse (Console.ReadLine ());
+int comp = 1;
+int a = 1;
+
+while (a <= N)
+{
+    comp = comp * a;
+    a++;
+}
+
+Console.WriteLine ("Произведение чисел от 1 до N = " +comp);
